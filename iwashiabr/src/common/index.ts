20943539b07c9e4d3b324cb6b9ea13367d44fa07
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-10-23 20:40:14 +0900

    add

---
+++ iwashiabr/src/common/index.ts
@@ -0,0 +1,7 @@
+// pattern
+export * from './pattern/observer';
+export * from './pattern/debounce';
+export * from './pattern/throttle';
+export * from './pattern/decorator';
+// function
+export * from './hello';
