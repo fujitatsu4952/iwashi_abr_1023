654a275faae21c92ff0e6c8ee97b2859e81f9553
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-10-24 17:43:45 +0900

    add

---
+++ iwashiabr/dist/user/guest/usecase/planUsecase.d.ts
@@ -0,0 +1,7 @@
+import { PlanMast, Scalars } from '../../../entity';
+export interface IGuestPlanUsecase {
+    getBlancMast(): Promise<PlanMast>;
+    addMast(planMast: PlanMast): Promise<any>;
+    updateMast(planMast: PlanMast): Promise<any>;
+    fetchPlanMasts(planID: Scalars['ID']): Promise<any | undefined>;
+}
