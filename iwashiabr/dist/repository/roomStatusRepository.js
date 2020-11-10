02b5a2e47fa293564ffbaf896b359db5509d376d
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-11-10 11:48:39 +0900

    add

--- iwashiabr/dist/repository/roomStatusRepository.js
+++ iwashiabr/dist/repository/roomStatusRepository.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.roomStatusRepository = void 0;
 const graphql_request_1 = require("graphql-request");
 const mutations_1 = require("./mutations");
-const graphqlClient = new graphql_request_1.GraphQLClient(`18.180.113.27:8080`);
+const graphqlClient = new graphql_request_1.GraphQLClient(`18.180.113.27`);
 const sdk = mutations_1.getSdk(graphqlClient);
 class roomStatusRepository {
     async updateRoomStatus(roomStatus) {
--- iwashiabr/src/repository/roomStatusRepository.ts
+++ iwashiabr/src/repository/roomStatusRepository.ts
@@ -3,7 +3,7 @@ import { request, GraphQLClient, gql } from "graphql-request";
 import { getSdk } from "./mutations";
 
 
-const graphqlClient = new GraphQLClient(`18.180.113.27:8080`);
+const graphqlClient = new GraphQLClient(`18.180.113.27`);
 const sdk = getSdk(graphqlClient);
 
 export class roomStatusRepository {
